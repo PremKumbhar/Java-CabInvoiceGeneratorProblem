4be4c8a929e893aa9e1a3bb1e832f9b80baf806a
PremKumbhar <kumbharprem7738@gmail.com>
2022-11-17 20:11:03 +0530

    [Prem]:Added invoice generator should return total fare journey

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -14,6 +14,17 @@ public class InvoiceServiceTest {
          * Calculating the fair
          */
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(25.0, fare, 0.0);
+        Assert.assertEquals(25.0, fare,0.0);
+    }
+    /*
+   Step 2 Test Case for minimum fare should give 5
+   */
+    @Test
+    public void givenDistanceAndTime_WhenTotalFareLessThan10_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5.0, fare,0.0);
     }
 }
